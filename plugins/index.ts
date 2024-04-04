17667f0a99bdcbe1a8d63ab9d31d05758aecf26d
Fabrice Francois <fabricevladimir@gmail.com>
2024-04-04 08:03:17 -0400

    removed unused plugins and their constants from list

--- plugins/index.ts
+++ plugins/index.ts
@@ -1,9 +1,4 @@
-import {
-  PUB_DELEGATION_CONTRACT_ADDRESS,
-  PUB_DUAL_GOVERNANCE_PLUGIN_ADDRESS,
-  PUB_TOKEN_VOTING_PLUGIN_ADDRESS,
-  PUB_LOCK_TO_VOTE_PLUGIN_ADDRESS,
-} from "@/constants";
+import { PUB_TOKEN_VOTING_PLUGIN_ADDRESS } from "@/constants";
 import { IconType } from "@aragon/ods";
 
 type PluginItem = {
@@ -19,31 +14,10 @@ type PluginItem = {
 
 export const plugins: PluginItem[] = [
   {
-    id: "core-proposals",
-    folderName: "dualGovernance",
-    title: "Core team proposals",
-    icon: IconType.APP_MEMBERS,
-    pluginAddress: PUB_DUAL_GOVERNANCE_PLUGIN_ADDRESS,
-  },
-  {
     id: "community-proposals",
     folderName: "tokenVoting",
     title: "Community proposals",
     icon: IconType.BLOCKCHAIN_BLOCKCHAIN,
     pluginAddress: PUB_TOKEN_VOTING_PLUGIN_ADDRESS,
-  },
-  {
-    id: "lock-to-vote",
-    folderName: "lockToVote",
-    title: "Morpho Vault Gov",
-    icon: IconType.BLOCKCHAIN_BLOCK,
-    pluginAddress: PUB_LOCK_TO_VOTE_PLUGIN_ADDRESS,
-  },
-  {
-    id: "delegate-wall",
-    folderName: "delegateAnnouncer",
-    title: "Delegation",
-    icon: IconType.FEEDBACK,
-    pluginAddress: PUB_DELEGATION_CONTRACT_ADDRESS,
   },
 ];
