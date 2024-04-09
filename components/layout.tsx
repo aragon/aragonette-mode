dc5ea4a6c4514dfca70a1ceb3e5578c494d79347
Fabrice Francois <fabricevladimir@gmail.com>
2024-04-09 09:17:23 -0400

    run linter and formatter

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,8 +4,8 @@ import { ReactNode } from "react";
 export function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="flex h-screen">
-      <div className="overflow-y-auto flex flex-col items-center w-full">
-        <div className="w-full md:w-4/5 lg:w-2/3 2xl:w-3/5 p-3">
+      <div className="flex w-full flex-col items-center overflow-y-auto">
+        <div className="w-full p-3 md:w-4/5 lg:w-2/3 2xl:w-3/5">
           <Header />
           {children}
         </div>
