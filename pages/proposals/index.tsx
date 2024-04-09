1c9604fe2f9abed14785556c37386d1027c6eea8
Fabrice Francois <fabricevladimir@gmail.com>
2024-04-09 17:50:45 -0400

    update page stubs

--- pages/proposals/index.tsx
+++ pages/proposals/index.tsx
@@ -1,3 +1,24 @@
+import Link from "next/link";
+import { Link as StyledLink } from "@aragon/ods";
+
 export default function Proposals() {
-  return <div>Proposals Page</div>;
+  const mockPips = [
+    { id: 1, title: "PIP 1", description: "This is a description for PIP 1" },
+    { id: 2, title: "PIP 2", description: "This is a description for PIP 2" },
+  ];
+
+  return (
+    <div>
+      <h1>Pips List Page</h1>
+      <div>
+        {mockPips.map((pip) => (
+          <div key={pip.id}>
+            <Link href={`/proposals/${pip.id}`}>
+              <StyledLink>{`${pip.title} - ${pip.description}`}</StyledLink>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
