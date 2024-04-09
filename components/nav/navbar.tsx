c08c54441b49afc81b0ca737f3fb172e8b43d0cb
Fabrice Francois <fabricevladimir@gmail.com>
2024-04-09 18:00:02 -0400

    update navbar links

--- components/nav/navbar.tsx
+++ components/nav/navbar.tsx
@@ -1,26 +1,21 @@
 import WalletContainer from "@/components/WalletContainer";
-import { plugins } from "@/plugins";
 import { AvatarIcon, IconType } from "@aragon/ods";
 import Image from "next/image";
 import Link from "next/link";
 import { NavLink } from "./navLink";
+import { Dashboard, Learn, Members, Proposals } from "./routes";
 
 export const Navbar: React.FC = () => {
-  const NavLinks = [
-    { path: "/", id: "dashboard", name: "Dashboard" },
-    ...plugins.map((p) => ({
-      id: p.id,
-      name: p.title,
-      path: `/plugins/${p.id}/#/`,
-    })),
-  ];
+  const NavLinks = [Dashboard, Proposals, Members, Learn];
 
   return (
     <nav className="sticky top-0 w-full flex-col gap-2 border-b border-b-neutral-100 bg-neutral-0 p-3 md:px-6 md:pb-0 md:pt-5 lg:gap-3">
       <div className="flex w-full items-center justify-between">
         <Link href="/" className="flex items-center gap-x-3">
           <Image src="/logo-bw-lg.png" width="60" height="60" className="w-9 shrink-0" alt="Aragonette" />
-          <span className="py-1 text-lg font-semibold leading-tight text-neutral-700 md:text-xl">Aragonette</span>
+          <span className="py-1 text-lg font-semibold leading-tight text-neutral-700 md:text-xl">
+            Polygon Governance Hub
+          </span>
         </Link>
 
         <div className="flex items-center gap-x-2">
