4966e87127abd2594bbd05ce0290147d2d46adcb
Fabrice Francois <fabricevladimir@gmail.com>
2024-04-10 11:22:58 -0400

    update navbar to include mobileNavDialog

--- components/nav/navbar.tsx
+++ components/nav/navbar.tsx
@@ -1,45 +1,60 @@
 import WalletContainer from "@/components/WalletContainer";
 import { AvatarIcon, IconType } from "@aragon/ods";
+import classNames from "classnames";
 import Image from "next/image";
 import Link from "next/link";
-import { NavLink } from "./navLink";
+import { NavLink, type INavLink } from "./navLink";
 import { Dashboard, Learn, Members, Proposals } from "./routes";
+import { useState } from "react";
+import { MobileNavDialog } from "./mobileNavDialog";
 
 export const Navbar: React.FC = () => {
-  const NavLinks = [Dashboard, Proposals, Members, Learn];
+  const [open, setOpen] = useState(false);
+  const navLinks: INavLink[] = [Dashboard, Proposals, Members, Learn];
 
   return (
-    <nav className="sticky top-0 w-full flex-col gap-2 border-b border-b-neutral-100 bg-neutral-0 p-3 md:px-6 md:pb-0 md:pt-5 lg:gap-3">
-      <div className="flex w-full items-center justify-between">
-        <Link href="/" className="flex items-center gap-x-3">
-          <Image src="/logo-bw-lg.png" width="60" height="60" className="w-9 shrink-0" alt="Aragonette" />
-          <span className="py-1 text-lg font-semibold leading-tight text-neutral-700 md:text-xl">
-            Polygon Governance Hub
-          </span>
-        </Link>
+    <>
+      <nav className="h-30 sticky top-0 w-full flex-col gap-2 border-b border-b-neutral-100 bg-neutral-0 p-3 md:px-6 md:pb-0 md:pt-5 lg:gap-3">
+        <div className="flex w-full items-center justify-between">
+          <Link
+            href="/"
+            className={classNames(
+              "flex items-center gap-x-3 rounded-full md:rounded-lg",
+              "outline-none focus:outline-none focus-visible:ring focus-visible:ring-primary focus-visible:ring-offset" // focus styles
+            )}
+          >
+            <Image src="/logo-bw-lg.png" width="60" height="60" className="w-9 shrink-0" alt="Aragonette" />
+            <span className="py-1 text-lg font-semibold leading-tight text-neutral-700 md:text-xl">
+              Polygon Governance Hub
+            </span>
+          </Link>
 
-        <div className="flex items-center gap-x-2">
-          <div className="shrink-0">
-            <WalletContainer />
+          <div className="flex items-center gap-x-2">
+            <div className="shrink-0">
+              <WalletContainer />
+            </div>
+
+            {/* Nav Trigger */}
+            <button
+              onClick={() => setOpen(true)}
+              className={classNames(
+                "rounded-full border border-neutral-100 bg-neutral-0 p-1 md:hidden",
+                "outline-none focus:outline-none focus-visible:ring focus-visible:ring-primary focus-visible:ring-offset" // focus styles
+              )}
+            >
+              <AvatarIcon size="lg" icon={IconType.MENU} />
+            </button>
           </div>
-
-          {/* Nav Trigger */}
-          <button className="rounded-full border border-neutral-100 bg-neutral-0 p-1 md:hidden">
-            <AvatarIcon size="lg" icon={IconType.MENU} />
-          </button>
         </div>
-      </div>
 
-      {/* Tab wrapper */}
-      <ul className="-mb-0.25 hidden gap-x-10 md:flex lg:pl-14">
-        {NavLinks.map((tab) => {
-          return (
-            <li key={tab.id}>
-              <NavLink label={tab.name} path={tab.path} id={tab.id} />
-            </li>
-          );
-        })}
-      </ul>
-    </nav>
+        {/* Tab wrapper */}
+        <ul className="hidden gap-x-10 md:flex lg:pl-14">
+          {navLinks.map(({ id, name, path }) => (
+            <NavLink name={name} path={path} id={id} key={id} />
+          ))}
+        </ul>
+      </nav>
+      <MobileNavDialog open={open} navLinks={navLinks} onOpenChange={setOpen} />
+    </>
   );
 };
