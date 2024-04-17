794f588d0f959f6aecdad3935394498f928d8fe3
Fabrice Francois <fabricevladimir@gmail.com>
2024-04-17 09:12:04 -0400

    add mutations and query options for proposal service

---
+++ features/proposals/services/proposal/query-options.ts
@@ -0,0 +1,34 @@
+import { queryOptions } from "@tanstack/react-query";
+import { type IFetchProposalListParams, type IFetchProposalParams, type IFetchVotesParams } from "./params";
+import { fetchProposal, fetchProposals, fetchVotes } from "./proposal-service-mock";
+
+// TODO: use live service
+// import { fetchProposal, fetchProposals, fetchVotes } from "./proposal-service";
+
+export const proposalKeys = {
+  all: ["proposals"] as const,
+  list: (params: IFetchProposalListParams) => [...proposalKeys.all, "list", params],
+  detail: (params: IFetchProposalParams) => [...proposalKeys.all, "details", params] as const,
+  votes: (params: IFetchVotesParams) => [...proposalKeys.all, "votes", params] as const,
+};
+
+export function proposalList(params: IFetchProposalListParams) {
+  return queryOptions({
+    queryKey: proposalKeys.list(params),
+    queryFn: () => fetchProposals(params),
+  });
+}
+
+export function proposal(params: IFetchProposalParams) {
+  return queryOptions({
+    queryKey: proposalKeys.detail(params),
+    queryFn: () => fetchProposal(params),
+  });
+}
+
+export function proposalVotes(params: IFetchVotesParams) {
+  return queryOptions({
+    queryKey: proposalKeys.votes(params),
+    queryFn: () => fetchVotes(params),
+  });
+}
