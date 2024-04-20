2396dfe89b3c8557c28b5227fe963abb6158d32d
Fabrice Francois <fabricevladimir@gmail.com>
2024-04-20 14:16:26 -0400

    update proposals page and layout

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -6,7 +6,9 @@ export const Layout: React.FC<{ children: ReactNode }> = (props) => {
     <div className="flex flex-col items-center gap-20">
       <div className="flex w-full flex-col items-center">
         <Navbar />
-        <div className="flex w-full flex-col px-4 py-6 md:p-6 xl:max-w-screen-xl xl:py-10">{props.children}</div>
+        <div className="flex w-full flex-col items-center px-4 py-6 md:p-6 xl:max-w-screen-xl xl:px-6 xl:py-10">
+          {props.children}
+        </div>
       </div>
 
       {/* Footer */}
--- pages/api/proposals.tsx
+++ pages/api/proposals.tsx
@@ -2,13 +2,9 @@ import { type IProposal } from "@/features/proposals";
 import { buildProposalResponse } from "@/features/proposals/providers/utils/proposal-builder";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-type ResponseData = {
-  proposals: IProposal[];
-};
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<IProposal[]>) {
   //TODO: Handle error cases
   const proposals = await buildProposalResponse();
 
-  res.status(200).json({ proposals });
+  res.status(200).json(proposals);
 }
