41956fb9465c556590299da26d88908cfcbc40da
Fabrice Francois <fabricevladimir@gmail.com>
2024-04-24 08:41:58 -0400

    add response type for proposals api

--- pages/api/proposals.tsx
+++ pages/api/proposals.tsx
@@ -1,10 +1,11 @@
 import { type IProposal } from "@/features/proposals";
 import { buildProposalResponse } from "@/features/proposals/providers/utils/proposal-builder";
+import { type IPaginatedResponse } from "@/utils/types";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<IProposal[]>) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<IPaginatedResponse<IProposal>>) {
   //TODO: Handle error cases
   const proposals = await buildProposalResponse();
 
-  res.status(200).json(proposals);
+  res.status(200).json({ data: proposals, pagination: { total: proposals.length } });
 }
