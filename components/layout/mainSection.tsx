1086781b8e8ad5cb2ed9f557eb75da1aa54a7a1c
Fabrice Francois <fabricevladimir@gmail.com>
2024-05-01 17:00:36 -0400

    update layouts

--- components/layout/mainSection.tsx
+++ components/layout/mainSection.tsx
@@ -1,5 +1,17 @@
-import { type ReactNode } from "react";
+import classNames from "classnames";
+import React, { type ReactNode } from "react";
 
-export function MainSection({ children }: { children: ReactNode }) {
-  return <main className="flex w-screen max-w-full flex-col items-center pt-6">{children}</main>;
+interface IMainSectionProps {
+  children?: ReactNode;
+  className?: string;
 }
+
+export const MainSection: React.FC<IMainSectionProps> = (props) => {
+  const { children, className } = props;
+
+  return (
+    <div className={classNames("mx-auto w-full max-w-screen-xl px-4 py-6 md:px-6 md:py-6 xl:px-6 xl:py-10", className)}>
+      {children}
+    </div>
+  );
+};
--- pages/proposals/index.tsx
+++ pages/proposals/index.tsx
@@ -1,18 +1,21 @@
+import { MainSection } from "@/components/layout/mainSection";
 import { ProposalDataList } from "@/features/proposals";
 import { Button, Heading, IconType } from "@aragon/ods";
 
 export default function Proposals() {
   return (
-    <div className="flex w-full max-w-[768px] flex-col items-center gap-y-6 md:px-6">
-      <div className="flex w-full gap-x-10">
-        <Heading as="h1" className="line-clamp-1 flex flex-1 shrink-0">
-          Polygon Improvement Proposals
-        </Heading>
-        <Button iconLeft={IconType.PLUS} size="lg" className="!rounded-full">
-          Onchain PIP
-        </Button>
+    <MainSection className="md:pb-20 xl:pb-20">
+      <div className="mx-auto flex w-full max-w-[768px] flex-col items-center gap-y-6 md:px-6">
+        <div className="flex w-full gap-x-10">
+          <Heading as="h1" className="line-clamp-1 flex flex-1 shrink-0">
+            Polygon Improvement Proposals
+          </Heading>
+          <Button iconLeft={IconType.PLUS} size="lg" className="!rounded-full">
+            Onchain PIP
+          </Button>
+        </div>
+        <ProposalDataList />
       </div>
-      <ProposalDataList />
-    </div>
+    </MainSection>
   );
 }
