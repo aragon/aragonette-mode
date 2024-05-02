8e8056afd3f5aa4e51b168795f615bd3bb740f80
Fabrice Francois <fabricevladimir@gmail.com>
2024-05-02 10:38:22 -0400

    add documentation and endDate formatting for proposal

--- features/proposals/services/proposal/selectors/toProposalDetails.ts
+++ features/proposals/services/proposal/selectors/toProposalDetails.ts
@@ -1,30 +1,63 @@
-import dayjs from "dayjs";
+import dayjs, { type Dayjs } from "dayjs";
 import { ProposalStages, type IProposal } from "../domain";
+import relativeTime from "dayjs/plugin/relativeTime";
 
 export type ProposalDetail = IProposal & {
   createdAt?: string;
   endDate?: string;
 };
 
+/**
+ * Transforms a proposal object into a more detailed structure, including formatted dates. It adds formatted
+ * creation and end dates to the proposal details, applying relative time formatting where appropriate.
+ *
+ * @param proposal - The proposal object to be transformed.
+ * @returns the transformed proposal object.
+ */
 export function toProposalDetails(proposal: IProposal | undefined): ProposalDetail | undefined {
+  dayjs.extend(relativeTime);
+
   if (!proposal) return;
 
-  const createdAt = parseCreatedAt(proposal.stages.find((stage) => stage.createdAt)?.createdAt);
+  const createdAt = parseDate(proposal.stages.find((stage) => stage.createdAt)?.createdAt)?.format("YYYY-MM-DD");
 
   // end date is specified on the Council Confirmation stage or
   // when proposal is an emergency -> the Council Approval stage
   const endDate =
-    proposal.stages.find((stage) => stage.id === ProposalStages.COUNCIL_CONFIRMATION && stage.endTimestamp)
-      ?.endTimestamp ??
-    proposal.stages.find((stage) => stage.id === ProposalStages.COUNCIL_APPROVAL && stage.endTimestamp)?.endTimestamp;
+    proposal.stages.find((stage) => stage.id === ProposalStages.COUNCIL_CONFIRMATION)?.voting?.endDate ??
+    proposal.stages.find((stage) => stage.id === ProposalStages.COUNCIL_APPROVAL)?.voting?.endDate;
 
-  return { ...proposal, createdAt, endDate };
+  const parsedEndDate = parseDate(endDate);
+
+  let formattedEndDate;
+
+  if (parsedEndDate) {
+    formattedEndDate = getSimpleRelativeTimeFromDate(parsedEndDate);
+  }
+
+  return { ...proposal, createdAt, endDate: formattedEndDate };
 }
 
-function parseCreatedAt(date: string | undefined): string {
-  if (date == null) return "unknown date";
+// parse a date string or unix timestamp as a string
+function parseDate(date: string | undefined): Dayjs | undefined {
+  if (date == null) return;
 
   const parsedInput = Number(date);
 
-  return (!isNaN(parsedInput) ? dayjs.unix(parsedInput) : dayjs(date)).format("YYYY-MM-DD");
+  return !isNaN(parsedInput) ? dayjs.unix(parsedInput) : dayjs(date);
+}
+
+// Function to get the simple relative time in days or weeks
+function getSimpleRelativeTimeFromDate(value: Dayjs) {
+  const now = dayjs();
+  const targetDate = dayjs(value);
+  const diffDays = targetDate.diff(now, "day");
+  const diffWeeks = targetDate.diff(now, "week");
+
+  // Decide whether to show days or weeks
+  if (Math.abs(diffWeeks) > 0 && Math.abs(diffDays) >= 7) {
+    return `${Math.abs(diffWeeks)} weeks`;
+  } else {
+    return `${Math.abs(diffDays)} days`;
+  }
 }
