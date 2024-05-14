d678622b36eef15b43406941d8efbf143322c085
Fabrice Francois <fabricevladimir@gmail.com>
2024-05-14 10:42:05 -0400

    add temp approval in proposal details

--- features/proposals/pages/ProposalDetails.tsx
+++ features/proposals/pages/ProposalDetails.tsx
@@ -1,21 +1,35 @@
 import { MainSection } from "@/components/layout/mainSection";
+import { useProposalApproval } from "@/plugins/multisig/hooks/useProposalApproval";
+import { useUserCanApprove } from "@/plugins/multisig/hooks/useUserCanApprove";
 import { generateBreadcrumbs } from "@/utils/nav";
-import { Card } from "@aragon/ods";
+import { Button, Card } from "@aragon/ods";
 import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/router";
 import { BodySection, CardResources, HeaderProposal, ProposalAction, TransparencyReport } from "../components";
+import { ProposalStages } from "../services";
 import { proposal as proposalQueryOptions } from "../services/proposal/query-options";
 
 export default function ProposalDetails() {
   const router = useRouter();
   const breadcrumbs = generateBreadcrumbs(router.asPath);
 
-  const { data: proposal, error } = useQuery(proposalQueryOptions({ proposalId: router.query.id as string }));
+  const proposalId = router.query.id as string;
+  const { data: proposal, error } = useQuery(proposalQueryOptions({ proposalId }));
+
+  // proposal id for current stage
+  const proposalVoteId = proposal?.stages?.find(
+    (stage) => stage.id === (proposal?.currentStage ?? ProposalStages.DRAFT)
+  )?.voting?.providerId;
+
+  // check if user can vote on a proposal the proposal
+  const userCanApprove = useUserCanApprove(proposalVoteId);
+  const { approveProposal, isConfirmed: isApproved } = useProposalApproval(proposalVoteId);
 
   if (proposal) {
-    const showActions = (proposal.actions?.length ?? 0) > 0;
-    const showVoting = false;
-    const showIncludedPIPS = (proposal.includedPips?.length ?? 0) > 0;
+    const isParentProposal = proposal.includedPips.length > 0;
+    const showActions = proposal.actions.length > 0;
+    const showVoting = true; // isParentProposal;
+    const showIncludedPIPS = isParentProposal;
 
     return (
       <>
@@ -25,7 +39,18 @@ export default function ProposalDetails() {
             {/* Proposal */}
             <div className="flex flex-col gap-y-6 md:w-[63%] md:shrink-0">
               {proposal.body && <BodySection body={proposal.body} />}
-              {showVoting && <Card>Voting terminal</Card>}
+              {showVoting && (
+                <Card className="p-6 shadow-neutral">
+                  <div className="flex flex-col gap-y-6">
+                    <h3>Voting Terminal</h3>
+                    <span>
+                      <Button onClick={approveProposal} disabled={!userCanApprove}>
+                        {isApproved ? "Approved" : "Approve"}
+                      </Button>
+                    </span>
+                  </div>
+                </Card>
+              )}
               {proposal.transparencyReport && <TransparencyReport report={proposal.transparencyReport} />}
               {showActions && <ProposalAction actions={proposal.actions} />}
             </div>
