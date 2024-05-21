32d425cda343aa40b81c18fdc421bdd1745836bc
Fabrice Francois <fabricevladimir@gmail.com>
2024-05-21 06:21:43 -0400

    add date utils

---
+++ utils/dates.ts
@@ -0,0 +1,19 @@
+import dayjs, { type Dayjs } from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+// Function to get the simple relative time in days or weeks
+export function getSimpleRelativeTimeFromDate(value: Dayjs) {
+  dayjs.extend(relativeTime);
+
+  const now = dayjs();
+  const targetDate = dayjs(value);
+  const diffDays = targetDate.diff(now, "day");
+  const diffWeeks = targetDate.diff(now, "week");
+
+  // Decide whether to show days or weeks
+  if (Math.abs(diffWeeks) > 0 && Math.abs(diffDays) >= 15) {
+    return `${Math.abs(diffWeeks)} ${Math.abs(diffWeeks) === 1 ? "week" : "weeks"}`;
+  } else {
+    return `${Math.abs(diffDays)} ${Math.abs(diffDays) === 1 ? "day" : "days"} `;
+  }
+}
