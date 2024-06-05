2caa6426c99129569d02de91618f19adeceb51d7
Fabrice Francois <fabricevladimir@gmail.com>
2024-06-05 13:25:13 -0400

    move voting to top of breakdown

--- features/proposals/components/proposalVoting/votingBreakdown/majorityVotingResult.tsx
+++ features/proposals/components/proposalVoting/votingBreakdown/majorityVotingResult.tsx
@@ -2,6 +2,7 @@ import { capitalizeFirstLetter } from "@/utils/case";
 import { Button, Heading, Progress, RadioCard, RadioGroup } from "@aragon/ods";
 import classNames from "classnames";
 import { useEffect, useState } from "react";
+import { DynamicVetoRateChart } from "./dynamicVetoRateChart";
 import { type VotingCta } from "./types";
 
 type Choices = "approve" | "reject" | "abstain";
@@ -9,6 +10,7 @@ type Choices = "approve" | "reject" | "abstain";
 export interface IBreakdownMajorityVotingResult {
   votingScores: { option: string; voteAmount: string; votePercentage: number; tokenSymbol: string }[];
   cta?: VotingCta;
+  proposalId: string;
 }
 
 const choiceClassNames: Record<Choices, string> = {
@@ -24,7 +26,7 @@ const choiceTextClassNames: Record<Choices, string> = {
 };
 
 export const BreakdownMajorityVotingResult: React.FC<IBreakdownMajorityVotingResult> = (props) => {
-  const { cta, votingScores } = props;
+  const { cta, votingScores, proposalId } = props;
 
   const [showOptions, setShowOptions] = useState(false);
   const [option, setOption] = useState<string>();
@@ -48,28 +50,6 @@ export const BreakdownMajorityVotingResult: React.FC<IBreakdownMajorityVotingRes
 
   return (
     <div className="flex flex-col gap-y-4">
-      <div className="flex flex-col gap-y-3 rounded-xl border border-neutral-100 p-3 shadow-neutral-sm md:flex-row md:gap-x-6 md:p-6">
-        {votingScores.map((choice, index) => (
-          <div className="flex flex-1 flex-col gap-y-3 md:flex-row md:gap-x-6" key={choice.option}>
-            <div className="flex flex-1 flex-col gap-y-2 py-1 md:gap-y-3 md:py-0">
-              <span
-                className={classNames(
-                  "capitalize leading-tight md:text-lg",
-                  choiceTextClassNames[choice.option as Choices]
-                )}
-              >
-                {choice.option}
-              </span>
-              <Progress value={choice.votePercentage} className={choiceClassNames[choice.option as Choices]} />
-              <div className="flex gap-x-1">
-                <span className="text-neutral-800">{choice.voteAmount}</span>
-                <span className="text-neutral-500">{choice.tokenSymbol}</span>
-              </div>
-            </div>
-            {index < votingScores.length - 1 && <div className="h-0.25 bg-neutral-100 md:h-auto md:w-0.25" />}
-          </div>
-        ))}
-      </div>
       {/* Options */}
       {showOptions && (
         <div className="flex flex-col gap-y-3 pt-3">
@@ -80,7 +60,7 @@ export const BreakdownMajorityVotingResult: React.FC<IBreakdownMajorityVotingRes
               completed, your vote will be counted and displayed.
             </p>
           </div>
-          <RadioGroup value={option} onValueChange={(value) => setOption(value)} className="!gap-y-3">
+          <RadioGroup value={option} onValueChange={(value) => setOption(value)}>
             {votingScores?.map((choice, index) => {
               const parsedChoice = capitalizeFirstLetter(choice.option);
               return (
@@ -115,6 +95,29 @@ export const BreakdownMajorityVotingResult: React.FC<IBreakdownMajorityVotingRes
           )}
         </div>
       )}
+      <div className="flex flex-col gap-y-3 rounded-xl border border-neutral-100 p-3 shadow-neutral-sm md:flex-row md:gap-x-6 md:p-6">
+        {votingScores.map((choice, index) => (
+          <div className="flex flex-1 flex-col gap-y-3 md:flex-row md:gap-x-6" key={choice.option}>
+            <div className="flex flex-1 flex-col gap-y-2 py-1 md:gap-y-3 md:py-0">
+              <span
+                className={classNames(
+                  "capitalize leading-tight md:text-lg",
+                  choiceTextClassNames[choice.option as Choices]
+                )}
+              >
+                {choice.option}
+              </span>
+              <Progress value={choice.votePercentage} className={choiceClassNames[choice.option as Choices]} />
+              <div className="flex gap-x-1">
+                <span className="text-neutral-800">{choice.voteAmount}</span>
+                <span className="text-neutral-500">{choice.tokenSymbol}</span>
+              </div>
+            </div>
+            {index < votingScores.length - 1 && <div className="h-0.25 bg-neutral-100 md:h-auto md:w-0.25" />}
+          </div>
+        ))}
+      </div>
+      <DynamicVetoRateChart proposalId={proposalId} />
     </div>
   );
 };
