3142553ff19856eeff1a9b3f884da4a2ccff0a8b
Fabrice Francois <fabricevladimir@gmail.com>
2024-06-05 13:24:49 -0400

    add conditional emergency voting terminal description

--- features/proposals/components/proposalVoting/proposalVoting.tsx
+++ features/proposals/components/proposalVoting/proposalVoting.tsx
@@ -11,21 +11,32 @@ dayjs.extend(relativeTime);
 
 interface IProposalVotingProps {
   stages: ITransformedStage[];
+  isEmergency: boolean;
 }
 
-export const ProposalVoting: React.FC<IProposalVotingProps> = ({ stages }) => {
+export const ProposalVoting: React.FC<IProposalVotingProps> = ({ isEmergency, stages }) => {
+  const defaultStage = stages.length === 1 ? ["Stage 1"] : [];
+
   return (
     <Card className="overflow-hidden rounded-xl bg-neutral-0 shadow-neutral">
       {/* Header */}
       <div className="flex flex-col gap-y-2 p-6">
         <Heading size="h2">Voting</Heading>
-        <p className="text-lg leading-normal text-neutral-500">
-          The proposal must first pass the Protocol Council Approval, then the community must vote in favour, and then
-          the Protocol Council confirms the onchain actions.
-        </p>
+        {!isEmergency && (
+          <p className="text-lg leading-normal text-neutral-500">
+            The proposal must first pass the Protocol Council Approval, then the community must vote in favour, and then
+            the Protocol Council confirms the onchain actions.
+          </p>
+        )}
+        {isEmergency && (
+          <p className="text-lg leading-normal text-neutral-500">
+            The Protocol Council must reach the required super-majority threshold in order to execute any onchain
+            actions.
+          </p>
+        )}
       </div>
       {/* Stages */}
-      <AccordionContainer isMulti={true} className="border-t border-t-neutral-100">
+      <AccordionContainer isMulti={true} className="border-t border-t-neutral-100" defaultValue={defaultStage}>
         {stages.map((stage, index) => (
           <VotingStage key={stage.id} {...({ ...stage, number: index + 1 } as IVotingStageProps)} />
         ))}
