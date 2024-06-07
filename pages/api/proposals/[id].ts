c75687c4f9ed1a3d183f8f71abbe6801eadc47a4
Fabrice Francois <fabricevladimir@gmail.com>
2024-06-07 19:21:46 -0400

    build fresh proposals

--- pages/api/proposals/[id].ts
+++ pages/api/proposals/[id].ts
@@ -1,7 +1,6 @@
-import { type IProposal, StageOrder, StageStatus, ProposalStages } from "@/features/proposals";
-import { buildProposalResponse, buildVotingResponse } from "@/features/proposals/providers";
+import { type IProposal } from "@/features/proposals";
+import { buildLiveProposalResponse } from "@/features/proposals/providers";
 import proposalRepository from "@/features/proposals/repository/proposal";
-import { logger } from "@/services/logger";
 import { checkParam } from "@/utils/api-utils";
 import { type IError } from "@/utils/types";
 import type { NextApiRequest, NextApiResponse } from "next";
@@ -17,42 +16,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
       return res.status(404).json({ error: { message: "Proposal not found" } });
     }
 
-    proposal.stages.sort((a, b) => {
-      return StageOrder[a.type] - StageOrder[b.type];
-    });
-
-    logger.info(`Building fresh proposal: ${parsedId}`);
-    const freshProposal = await buildProposalResponse(proposal);
+    const freshProposal = await buildLiveProposalResponse(proposal);
     await proposalRepository.upsertProposal(freshProposal);
-    proposal = freshProposal;
 
-    // for (const [index, stage] of proposal.stages.entries()) {
-    //   //TODO: Check if active after fixing dates/statuses [new Date(stage.voting.endDate) < new Date()]?
-    //   const res = await buildVotingResponse(stage);
-    //   if (res) {
-    //     const [voting, status, overallStatus] = res;
-    //     // TODO: Update stage and proposal statuses in the database
-    //     stage.voting = voting;
-    //     stage.status = status;
-    //     proposal.status = overallStatus;
-    //     if (
-    //       status === StageStatus.APPROVED &&
-    //       stage.type != ProposalStages.COUNCIL_CONFIRMATION &&
-    //       !proposal.isEmergency &&
-    //       index === proposal.stages.length - 1
-    //     ) {
-    //       try {
-    //         logger.info("Building fresh proposal");
-    //         const freshProposal = await buildProposalResponse(proposal);
-    //         await proposalRepository.upsertProposal(freshProposal);
-    //         proposal = freshProposal;
-    //         logger.info("Saving fresh proposal");
-    //       } catch (error) {
-    //         logger.error("Failed to update proposal", error);
-    //       }
-    //     }
-    //   }
-    // }
+    proposal = freshProposal;
 
     res.status(200).json(proposal);
   } catch (error) {
