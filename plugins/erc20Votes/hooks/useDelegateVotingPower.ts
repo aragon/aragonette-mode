efc8d18e18dc1cf42949a4ad470579d5fdbc886b
Fabrice Francois <fabricevladimir@gmail.com>
2024-06-20 07:50:52 -0400

    add delegation hook

---
+++ plugins/erc20Votes/hooks/useDelegateVotingPower.ts
@@ -0,0 +1,67 @@
+import { Erc20VotesAbi } from "@/artifacts/ERC20Votes.sol";
+import { PUB_TOKEN_ADDRESS } from "@/constants";
+import { useAlerts } from "@/context/Alerts";
+import { logger } from "@/services/logger";
+import { useCallback, useEffect } from "react";
+import { type Address } from "viem";
+import { useWaitForTransactionReceipt, useWriteContract } from "wagmi";
+
+export const useDelegateVotingPower = (onSuccess?: () => void) => {
+  const { addAlert } = useAlerts();
+  const { writeContract, data: hash, error, status } = useWriteContract();
+  const { isLoading: isConfirming, isSuccess: isConfirmed } = useWaitForTransactionReceipt({ hash });
+
+  useEffect(() => {
+    if (status === "idle" || status === "pending") return;
+    else if (status === "error") {
+      if (error?.message?.startsWith("User rejected the request")) {
+        addAlert("Transaction rejected by the user", {
+          timeout: 4 * 1000,
+        });
+      } else {
+        logger.error("Could not delegate voting power", error);
+        addAlert("Could not delegate voting power", { type: "error" });
+      }
+      return;
+    }
+
+    // success
+    if (!hash) return;
+    else if (isConfirming) {
+      addAlert("Delegation submitted", {
+        description: "Waiting for the transaction to be validated",
+        txHash: hash,
+      });
+      return;
+    } else if (!isConfirmed) return;
+
+    addAlert("Delegation registered", {
+      description: "The transaction has been validated",
+      type: "success",
+      txHash: hash,
+    });
+
+    onSuccess?.();
+  }, [status, hash, isConfirming, isConfirmed]);
+
+  const delegate = useCallback(
+    async (address: Address | undefined) => {
+      if (address) {
+        writeContract({
+          abi: Erc20VotesAbi,
+          address: PUB_TOKEN_ADDRESS,
+          functionName: "delegate",
+          args: [address],
+        });
+      }
+    },
+    [writeContract]
+  );
+
+  return {
+    announceDelegation: delegate,
+    isConfirmed,
+    isConfirming,
+    status,
+  };
+};
