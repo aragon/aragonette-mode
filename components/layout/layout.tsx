3a6757cd134efc06c51f647e2ea9afe90ffb5691
Fabrice Francois <fabricevladimir@gmail.com>
2024-07-11 07:37:40 -0400

    add footer to layout

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,15 +1,16 @@
 import { type ReactNode } from "react";
 import { Navbar } from "../nav/navbar";
+import { Footer } from "../footer/footer";
 
 export const Layout: React.FC<{ children: ReactNode }> = (props) => {
   return (
-    <div className="flex flex-col items-center gap-20">
+    <div className="flex min-h-screen flex-col items-center justify-between">
       <div className="w-full">
         <Navbar />
         {props.children}
       </div>
 
-      {/* Footer */}
+      <Footer />
     </div>
   );
 };
