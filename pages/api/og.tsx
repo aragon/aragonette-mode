5a5d5f0f0d67cebe81657473ee0c2835c4d4edaf
Fabrice Francois <fabricevladimir@gmail.com>
2024-07-24 12:42:46 -0400

    update Polygon logo size

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -26,10 +26,10 @@ export default async function handler() {
           style={{
             display: "flex",
             flexDirection: "column",
-            gap: 8,
+            gap: 20,
           }}
         >
-          <svg width="820" height="160" viewBox="0 0 205 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg width="640" height="124" viewBox="0 0 205 40" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g clipPath="url(#clip0_1_39960)">
               <path
                 d="M31.882 0L19.4018 7.1676V29.5381L12.5158 33.5297L5.58775 29.5349V21.5482L12.5158 17.5921L16.9705 20.1755V13.7136L12.477 11.1626L0 18.4111V32.7496L12.5191 39.9593L24.996 32.7496V10.3824L31.924 6.3874L38.8489 10.3824V18.3334L31.924 22.364L27.4306 19.7579V26.1873L31.882 28.7545L44.482 21.587V7.1676L31.882 0Z"
