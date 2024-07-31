776e82f4d7c9c36c312b1904433fd25c370aada7
Fabrice Francois <fabricevladimir@gmail.com>
2024-07-31 10:19:21 -0400

    empty state for upcoming events

--- components/upcomingEvents/upcomingEvents.tsx
+++ components/upcomingEvents/upcomingEvents.tsx
@@ -1,13 +1,4 @@
-import { EventDataList } from "@/features/events/components/eventDataList/eventDataList";
-import { type IResource } from "@/utils/types";
-import { Card, Heading, IconType, Link } from "@aragon/ods";
-
-const resources: IResource[] = [
-  { name: "Governance Forum", link: "0", description: "Short description" },
-  { name: "Twitter", link: "1", description: "Short description" },
-  { name: "Farcaster", link: "2", description: "Short description" },
-  { name: "Github", link: "5", description: "Short description" },
-];
+import { EmptyState, Heading } from "@aragon/ods";
 
 export const UpcomingEvents = () => {
   return (
@@ -15,25 +6,12 @@ export const UpcomingEvents = () => {
       <div className="flex w-full flex-col gap-y-4">
         <div className="flex w-full flex-col gap-y-3">
           <Heading size="h2">Upcoming events</Heading>
-          <EventDataList />
+          <EmptyState
+            objectIllustration={{ object: "BUILD" }}
+            heading="Coming soon!"
+            className="rounded-xl bg-neutral-0"
+          />
         </div>
-      </div>
-      <div className="flex flex-col gap-y-4">
-        <Heading size="h2">Links</Heading>
-        <Card className="flex flex-col gap-y-4 bg-neutral-0 p-6 shadow-neutral">
-          {resources.map((r) => (
-            <Link
-              key={r.link}
-              href={r.link}
-              iconRight={IconType.LINK_EXTERNAL}
-              description={r.description}
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              {r.name}
-            </Link>
-          ))}
-        </Card>
       </div>
     </section>
   );
