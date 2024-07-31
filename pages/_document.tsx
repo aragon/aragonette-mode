7ecbf1401a81e803b238428af7091fbe0d723051
Fabrice Francois <fabricevladimir@gmail.com>
2024-07-31 13:22:14 -0400

    switch favicon type

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" sizes="any" />
+        <link rel="icon" type="image/svg+xml" href="/favicon-32x32.png" sizes="any" />
       </Head>
       <body className="bg-neutral-50">
         <Main />
