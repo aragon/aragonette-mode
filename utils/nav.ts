a546a9186b658ec76f5b32cbaac7a52fce80fd60
xavikh <xavikh@gmail.com>
2024-07-31 19:19:35 +0200

    Shorten breadcrumb

--- utils/nav.ts
+++ utils/nav.ts
@@ -5,11 +5,16 @@ export function generateBreadcrumbs(pathname: string): IBreadcrumbsLink[] {
   const paths = pathname.split("/").filter(Boolean);
   let pathAccumulator = "";
 
-  return paths.map((path) => {
-    pathAccumulator += `/${path}`;
-    return {
-      label: capitalizeFirstLetter(path),
-      href: pathAccumulator,
-    };
-  });
+  return paths
+    .map((path) => {
+      path = path.length > 32 ? path.substring(0, 6) + "..." + path.substring(path.length - 4) : path;
+      return path;
+    })
+    .map((path) => {
+      pathAccumulator += `/${path}`;
+      return {
+        label: capitalizeFirstLetter(path),
+        href: pathAccumulator,
+      };
+    });
 }
