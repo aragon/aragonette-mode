d764d6667b1aea250b0c9de9be17c064af42d2a6
Fabrice Francois <fabricevladimir@gmail.com>
2024-08-02 13:00:26 -0400

    padding to not found and authorized

--- components/unAuthorized/unAuthorized.tsx
+++ components/unAuthorized/unAuthorized.tsx
@@ -6,9 +6,9 @@ export default function Unauthorized() {
   const router = useRouter();
 
   return (
-    <div className="flex w-full flex-col items-center justify-center pt-60">
+    <div className="flex w-full flex-col items-center justify-center pt-32 md:pt-60">
       <Heading size="h1">Unauthorized</Heading>
-      <p className="text-xl text-neutral-800">You do not have access to this page.</p>
+      <p className="text-center text-xl text-neutral-800">You do not have access to this page.</p>
       <Button className="mt-6 !rounded-full" variant="tertiary" onClick={() => router.push(Dashboard.path)}>
         Go to Dashboard
       </Button>
