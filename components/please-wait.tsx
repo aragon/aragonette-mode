bf7ccbbc2d6d10acb40974eb6307d0aeac022d2e
Fabrice Francois <fabricevladimir@gmail.com>
2024-08-02 06:05:26 -0400

    add please wait spinner

--- components/please-wait.tsx
+++ components/please-wait.tsx
@@ -4,7 +4,7 @@ export const PleaseWaitSpinner = ({ status = "Loading", fullMessage }: { status?
   const message = fullMessage ?? `${status}, please wait...`;
 
   return (
-    <div>
+    <div className="text-neutral-500">
       <Spinner size="sm" variant="neutral" className="-m-[2px] inline-block" /> &nbsp;&nbsp;{message}
     </div>
   );
