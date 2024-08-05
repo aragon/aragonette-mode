f36780b7c27bb791c92824b70ba679a153c5987b
xavikh <xavikh@gmail.com>
2024-08-05 12:46:17 +0200

    Update PUB_API_BASE_URL default

--- constants.ts
+++ constants.ts
@@ -22,7 +22,7 @@ export const PUB_WALLET_CONNECT_PROJECT_ID = process.env.NEXT_PUBLIC_WALLET_CONN
 export const PUB_IPFS_ENDPOINT = process.env.NEXT_PUBLIC_IPFS_ENDPOINT ?? "";
 export const PUB_IPFS_API_KEY = process.env.NEXT_PUBLIC_IPFS_API_KEY ?? "";
 
-export const PUB_API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:3000/api";
+export const PUB_API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? "/api";
 export const PUB_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL ?? "http://localhost:3000/";
 export const AUTH_API_TOKEN = process.env.AUTH_API_TOKEN ?? "";
 
