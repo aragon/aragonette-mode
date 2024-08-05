1559c2acc306ef1024fa1a21c7549a205b2e34cf
xavikh <xavikh@gmail.com>
2024-08-05 12:43:15 +0200

    Reduce "Governed on" size

--- components/nav/navbar.tsx
+++ components/nav/navbar.tsx
@@ -38,8 +38,8 @@ export const Navbar: React.FC = () => {
               >
                 <Image
                   src="/mode-green.svg"
-                  width="120"
-                  height="36"
+                  width="150"
+                  height="45"
                   className="hidden shrink-0 sm:block"
                   alt="Mode"
                   priority={true}
