711afd89d33457d4f3fdbd175232c39631fb7428
xavikh <xavikh@gmail.com>
2024-08-08 10:32:11 +0200

    Temporary add council addresses

--- council.ts
+++ council.ts
@@ -12,4 +12,6 @@ export type ICouncilMemberDataListItem = {
 
 export const COUNCIL_MEMBERS: ICouncilMemberDataListItem[] = [
   { address: "0x8bF1e340055c7dE62F11229A149d3A1918de3d74" },
+  { address: "0x376c649111543C46Ce15fD3a9386b4F202A6E06c" },
+  { address: "0x35911Cc89aaBe7Af6726046823D5b678B6A1498d" },
 ];
