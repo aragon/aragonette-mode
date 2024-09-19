fda553d0d90aa057e7fa647e4e862dec58f7a5b1
Jør∂¡ <itsbrickpop@gmail.com>
2024-09-19 16:33:34 +0100

    Minor adjustments

--- components/dashboard/header.tsx
+++ components/dashboard/header.tsx
@@ -1,14 +1,17 @@
+import React from "react";
 import { formatterUtils, NumberFormat } from "@aragon/ods";
 
-export const DashboardHeader = (props: any) => {
+export const DashboardHeader = (props: { count: number }) => {
   const totalProposals = props.count;
 
   return (
     <header className="relative flex w-full justify-center bg-gradient-to-b from-neutral-0 to-transparent">
       {/* Radial gradients */}
-      <section className="bg-ellipse-37 absolute -top-[36px] -z-10 size-[180px] rounded-full blur-[120px] sm:right-[80px] sm:size-[320px]" />
-      <section className="bg-ellipse-38 absolute left-[68px] top-[170px] -z-10 size-[250px] rounded-full blur-[80px] sm:size-[400px]" />
-      <section className="bg-ellipse-39 absolute right-[400px] top-[153px] -z-10 hidden size-[540px] rounded-full blur-[120px] lg:block" />
+      <div>
+        {/* <section className="bg-ellipse-37 absolute -top-[36px] -z-10 size-[180px] rounded-full blur-[120px] sm:right-[80px] sm:size-[320px]" /> */}
+        <section className="absolute left-[68px] top-[170px] -z-10 size-[250px] rounded-full bg-ellipse-38 blur-[80px] sm:size-[400px]" />
+        <section className="absolute right-[400px] top-[153px] -z-10 hidden size-[540px] rounded-full bg-ellipse-39 blur-[120px] lg:block" />
+      </div>
 
       <div className="flex w-full max-w-screen-xl flex-col gap-y-8 pb-8 pt-8 md:gap-y-12 md:pt-8">
         <div className="flex flex-col gap-y-8">
@@ -24,7 +27,7 @@ export const DashboardHeader = (props: any) => {
         <div className="flex flex-col gap-x-20 gap-y-6 sm:flex-row md:w-4/5">
           {/* Proposal count */}
           <div className="flex flex-col">
-            <span className="text-3xl text-primary-400 md:text-4xl">
+            <span className="title text-3xl text-primary-400 md:text-4xl">
               {formatterUtils.formatNumber(totalProposals, { format: NumberFormat.GENERIC_SHORT })}
             </span>
             <span className="text-xl text-neutral-500">{totalProposals === 1 ? "Proposal" : "Proposals"}</span>
@@ -32,7 +35,7 @@ export const DashboardHeader = (props: any) => {
 
           {/* TVL */}
           <div className="flex flex-col">
-            <div className="flex items-baseline gap-x-1">
+            <div className="title flex items-baseline gap-x-1">
               <span className="text-3xl text-primary-400 md:text-4xl">$500M</span>
             </div>
             <span className="text-xl text-neutral-500">TVL</span>
@@ -40,7 +43,7 @@ export const DashboardHeader = (props: any) => {
 
           {/* Token holders */}
           <div className="flex flex-col">
-            <div className="flex items-baseline gap-x-1">
+            <div className="title flex items-baseline gap-x-1">
               <span className="text-3xl text-primary-400 md:text-4xl">
                 {formatterUtils.formatNumber(96500, {
                   format: NumberFormat.GENERIC_SHORT,
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,87 +1,51 @@
-import { FeaturedArticles } from "@/components/featuredArticles/featuredArticles";
-import { HeaderDao } from "@/components/headerDao/headerDao";
-import { LatestProposals } from "@/components/latestProposals/latestProposals";
-import { UpcomingEvents } from "@/components/upcomingEvents/upcomingEvents";
-import { PUB_DISCORD_URL } from "@/constants";
-import { proposalsCount } from "@/features/proposals";
-import { queryClient } from "@/utils/query-client";
-import { type IResource } from "@/utils/types";
-import { Button, Card, Heading } from "@aragon/ods";
-import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
+import React from "react";
+import { MainSection } from "@/components/layout/main-section";
+import { Button, Card, IllustrationHuman } from "@aragon/ods";
+import { useAccount } from "wagmi";
+import { useWeb3Modal } from "@web3modal/wagmi/react";
+import { DashboardHeader } from "@/components/dashboard/header";
+import { DashboardResources } from "@/components/dashboard/resources";
 
-type IDashboardResource = IResource & { cta: string; primary?: boolean };
+export default function StandardHome() {
+  const proposalCount = 0;
 
-const resources: IDashboardResource[] = [
-  {
-    name: "Governance Forum",
-    link: "https://modenetwork.notion.site",
-    description:
-      "Visit the Mode governance forum to see the latest developments in the community. Comment on or submit proposals for partnerships, services, or general protocol improvements.",
-    cta: "Visit",
-  },
-  {
-    name: "Discussion",
-    link: PUB_DISCORD_URL,
-    description:
-      "For general inquiries, active debate and discussion for all things Mode. Chat about Governance, Building or Discovering the latest opportunities on Mode.",
-    cta: "Join Discord",
-  },
-  {
-    name: "Developers",
-    link: "https://www.mode.network/developers",
-    description:
-      "Build on Mode: Push the limits with our community of hungry and talented builders. What innovations are you working on?",
-    cta: "Start building",
-  },
-];
-
-export const getServerSideProps = (async () => {
-  const count = await queryClient.fetchQuery(proposalsCount());
-
-  return {
-    props: {
-      count,
-    },
-  };
-}) satisfies GetServerSideProps<{ count: number }>;
-
-export default function Home(props: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <>
-      <HeaderDao count={props.count} />
-      <main className="mx-auto max-w-screen-xl">
-        <div className="px-4 pb-6 pt-10 md:px-6 md:pb-16">
-          <div className="grid grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))] gap-6 sm:grid-cols-[repeat(auto-fill,_minmax(350px,_1fr))] md:gap-6">
-            {resources.map((resource) => (
-              <Card
-                key={resource.link}
-                className="flex flex-col justify-between gap-y-6 bg-neutral-0 p-6 shadow-neutral-md"
-              >
-                <Heading size="h2">{resource.name}</Heading>
-                <div className="flex grow flex-col justify-start">
-                  <p className="text-neutral-500">{resource.description}</p>
-                </div>
-                <span className="flex">
-                  <Button
-                    href={resource.link}
-                    variant={resource.primary ? "primary" : "secondary"}
-                    className="!rounded-full"
-                    rel="noopener noreferrer"
-                    target="_blank"
-                  >
-                    {resource.cta}
-                  </Button>
-                </span>
-              </Card>
-            ))}
-          </div>
-        </div>
-        <div className="flex flex-col gap-y-10 px-4 pb-6 pt-10 md:flex-row md:gap-x-12 md:px-6 md:pb-12">
-          <LatestProposals />
-          <UpcomingEvents />
-        </div>
-        <FeaturedArticles />
-      </main>
+      <MainSection>
+        <DashboardHeader count={proposalCount} />
+        <DashboardResources />
+      </MainSection>
+      <ConnectWidget />
     </>
   );
 }
+
+// helpers
+
+const ConnectWidget = () => {
+  const { isConnected } = useAccount();
+  const { open } = useWeb3Modal();
+
+  if (isConnected) return null;
+
+  return (
+    <MainSection narrow>
+      <Card className="flex flex-col justify-between gap-y-6 bg-neutral-0 p-6 shadow-neutral-md">
+        <h1 className="line-clamp-1 flex flex-1 shrink-0 text-2xl font-normal leading-tight text-neutral-800 md:text-3xl">
+          Connect your wallet
+        </h1>
+        <p className="text-md text-neutral-400">
+          Get started by connecting your wallet and selecting a section from the menu.
+        </p>
+        <div className="">
+          <IllustrationHuman className="mx-auto mb-10 max-w-96" body="BLOCKS" expression="SMILE_WINK" hairs="CURLY" />
+          <div className="flex justify-center">
+            <Button size="md" variant="primary" onClick={() => open()}>
+              <span>Connect wallet</span>
+            </Button>
+          </div>
+        </div>
+      </Card>
+    </MainSection>
+  );
+};
