1b92fb2ea5c953661723f8382f39b5cc3be243c7
Jør∂¡ <itsbrickpop@gmail.com>
2024-09-20 13:11:29 +0100

    Staking markup

--- plugins/stake/index.tsx
+++ plugins/stake/index.tsx
@@ -4,7 +4,7 @@ import {
   Button,
   Card,
   IconType,
-  InputNumber,
+  InputNumberMax,
   Link,
   TabsContent,
   TabsList,
@@ -33,19 +33,16 @@ export default function PluginPage() {
           </Link>
         </p>
       </div>
-      <SpreadRow>
+
+      <div className="grid grid-cols-1 gap-x-4 md:grid-cols-2">
         <Card className="mt-4 w-full p-8">
           <TabsRoot defaultValue="mode">
             <TabsList>
-              <TabsTrigger label="MODE" value="mode" />
-              <TabsTrigger label="Both" value="both" />
-              <TabsTrigger label="BPT" value="bpt" />
+              <TabsTrigger label="Stake MODE" value="mode" />
+              <TabsTrigger label="Stake BPT" value="bpt" />
             </TabsList>
             <TabsContent value="mode">
               <StakeToken name="MODE" address={PUB_MODE_TOKEN_ADDRESS} balance={1234n} />
-            </TabsContent>
-            <TabsContent value="both">
-              <div className="flex h-24 w-full items-center justify-center">Item 2 Content</div>
             </TabsContent>
             <TabsContent value="bpt">
               <StakeToken name="BPT" address={PUB_BPT_TOKEN_ADDRESS} balance={5555n} />
@@ -53,8 +50,38 @@ export default function PluginPage() {
           </TabsRoot>
         </Card>
 
-        <div className="">HI</div>
-      </SpreadRow>
+        <aside className="flex w-full flex-col gap-y-4 justify-self-center md:max-w-[370px] md:gap-y-6">
+          <dl className="divide-y divide-neutral-100">
+            <div className="flex flex-col items-baseline gap-y-2 py-3 lg:gap-x-6 lg:py-4">
+              <dt className="line-clamp-1 shrink-0 text-lg leading-tight text-neutral-800 lg:line-clamp-6">
+                Your active voting power
+              </dt>
+            </div>
+
+            <div className="grid grid-cols-2 gap-y-3 py-3">
+              <div>MODE</div>
+              <div>1234</div>
+            </div>
+
+            <div className="grid grid-cols-2 gap-y-3 py-3">
+              <div>BPT</div>
+              <div>5555</div>
+            </div>
+            <div className="grid grid-cols-2 gap-y-3 py-3">
+              <div>Total</div>
+              <div>6789</div>
+            </div>
+          </dl>
+          <div className="grid grid-cols-2 gap-3 py-3">
+            <Button variant="secondary" size="md" iconRight={IconType.LINK_EXTERNAL}>
+              Get more MODE
+            </Button>
+            <Button variant="secondary" size="md" iconRight={IconType.LINK_EXTERNAL}>
+              Get more BPT
+            </Button>
+          </div>
+        </aside>
+      </div>
     </MainSection>
   );
 }
@@ -64,17 +91,24 @@ export default function PluginPage() {
 type PercentValues = "" | "0" | "25" | "50" | "75" | "100";
 
 const StakeToken = ({ name, address, balance }: { name: string; address: Address; balance: bigint }) => {
-  const [balanceToStake, setBalanceToStake] = useState<bigint>();
-  const [percentToggle, setPercentToggle] = useState<PercentValues>("100");
+  const [balanceToStake, setBalanceToStake] = useState<bigint>(0n);
+  const [percentToggle, setPercentToggle] = useState<PercentValues>("0");
 
   const onBalanceEnter = (newBalance: string) => {
     const newValue = BigInt(newBalance);
 
-    if (newValue > balance) setBalanceToStake(balance);
-    else setBalanceToStake(newValue);
-
-    setPercentToggle("");
+    if (newValue > balance) {
+      setBalanceToStake(balance);
+      setPercentToggle("100");
+    } else {
+      setBalanceToStake(newValue);
+    }
   };
+
+  useEffect(() => {
+    setBalanceToStake(0n);
+    setPercentToggle("0");
+  }, [balance]);
 
   useEffect(() => {
     if (!percentToggle) return;
@@ -88,16 +122,16 @@ const StakeToken = ({ name, address, balance }: { name: string; address: Address
   };
 
   return (
-    <div className="mt-4 flex w-full max-w-[50%] flex-col gap-4">
-      <InputNumber
+    <div className="mt-4 flex w-full flex-col gap-4">
+      <InputNumberMax
+        max={Number(balance)}
         value={balanceToStake?.toString() || ""}
         onChange={(v) => onBalanceEnter(v || "0")}
-        placeholder={balance.toString() + " " + name}
       />
 
       <ToggleGroup
         isMultiSelect={false}
-        onChange={(v) => setPercentToggle(v as any)}
+        onChange={(v) => setPercentToggle(v as PercentValues)}
         value={percentToggle}
         className="flex justify-between"
       >
@@ -113,7 +147,7 @@ const StakeToken = ({ name, address, balance }: { name: string; address: Address
       </p>
 
       <Button className="mt-4 w-full" onClick={stake}>
-        Stake MODE
+        Stake {name}
       </Button>
     </div>
   );
