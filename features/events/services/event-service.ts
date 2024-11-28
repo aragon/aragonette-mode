2f0cea08e2fe37ac0634805956703e6cf1f15af9
gbonumore <gabrielebonumore@gmail.com>
2024-11-28 17:10:58 +0100

    port calendar component to mode

---
+++ features/events/services/event-service.ts
@@ -0,0 +1,16 @@
+import { PUB_API_BASE_URL } from "@/constants";
+import { type IListEventsParams } from "@/services/google/calendar";
+import { encodeSearchParams } from "@/utils/query";
+import { type IInfiniteDataResponse } from "@/utils/types";
+import { type calendar_v3 } from "@googleapis/calendar";
+
+class EventService {
+  async fetchEvents(params: IListEventsParams) {
+    const url = encodeSearchParams(`${PUB_API_BASE_URL}/calendar/`, params);
+    const response = await fetch(url);
+    const parsed: IInfiniteDataResponse<calendar_v3.Schema$Event> = await response.json();
+    return parsed;
+  }
+}
+
+export const eventService = new EventService();
