e022db523789c0f4c9d47515546d189880537e4e
gbonumore <gabrielebonumore@gmail.com>
2025-01-15 16:05:35 +0100

    RD-1120: section header

--- plugins/stake/components/section-header/index.tsx
+++ plugins/stake/components/section-header/index.tsx
@@ -11,13 +11,17 @@ interface IHeaderProps {
 export const SectionHeader: React.FC<IHeaderProps> = ({ title, children, learnMoreUrl }) => {
   return (
     <div className="flex flex-col gap-y-3">
-      <h1 className="line-clamp-1 flex flex-1 shrink-0 text-3xl font-normal leading-tight text-neutral-800 md:text-3xl">
-        {title}
-      </h1>
+      {title ? (
+        <h1 className="line-clamp-1 flex flex-1 shrink-0 text-3xl font-normal leading-tight text-neutral-800 md:text-3xl">
+          {title}
+        </h1>
+      ) : null}
       <p className="max-w-[700px]">{children}</p>
-      <Link target="_blank" href={learnMoreUrl} variant="primary" iconRight={IconType.LINK_EXTERNAL}>
-        Learn more
-      </Link>
+      {learnMoreUrl ? (
+        <Link target="_blank" href={learnMoreUrl} variant="primary" iconRight={IconType.LINK_EXTERNAL}>
+          Learn more
+        </Link>
+      ) : null}
     </div>
   );
 };
